6a8cd3d384db12a38bea57b2f19a39534714a19b
Jamie STevens <jamiestevens2000@gmail.com>
2020-12-28 16:43:33 +0000

    Innit Spring project

---
+++ src/main/java/com/comp3200/MyPHRPlusBackend/MyPhrPlusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.comp3200.MyPHRPlusBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhrPlusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPhrPlusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comp3200/MyPHRPlusBackend/MyPhrPlusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp3200.MyPHRPlusBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhrPlusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
