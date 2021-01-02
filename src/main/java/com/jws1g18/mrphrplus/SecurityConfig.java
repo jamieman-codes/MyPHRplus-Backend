b56e4adf1fae4961e7556108a9417ca7377f50d6
Jamie STevens <jamiestevens2000@gmail.com>
2021-01-02 14:08:18 +0000

    Re-init Spring project. Added hello world

---
+++ src/main/java/com/jws1g18/mrphrplus/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.jws1g18.mrphrplus;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security.httpBasic().disable();
+    }
+}
--- src/test/java/com/jws1g18/mrphrplus/MrphrplusApplicationTests.java
+++ src/test/java/com/jws1g18/mrphrplus/MrphrplusApplicationTests.java
@@ -1,10 +1,10 @@
-package com.comp3200.MyPHRPlusBackend;
+package com.jws1g18.mrphrplus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyPhrPlusBackendApplicationTests {
+class MrphrplusApplicationTests {
 
 	@Test
 	void contextLoads() {
